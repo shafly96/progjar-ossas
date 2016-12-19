3f27beb089ff6bb962534004e768ccc9c3b8315f
Livingroom-PC\Nanda <nanda.ricky10@gmail.com>
2016-12-19 14:25:43 +0700

    client update

---
+++ element/client.py
@@ -0,0 +1,113 @@
+import pygame
+from time import sleep, time
+
+class Elemental():
+	def __init__(self):
+		#membuat kotak
+		pygame.init()
+		self.screen = pygame.display.set_mode((690, 400))
+		pygame.display.set_caption("Elemental Fight")
+		self.clock = pygame.time.Clock()
+
+		#inisialisasi
+		self.running = True
+
+		#inisialisasi posisi client ketika klik element
+		self.xpos = -1
+		self.ypos = -1
+
+		#membuat 60 fps
+		self.clock.tick(60)
+
+		#self.tunggu()
+
+		#warna bg
+		self.screen.fill((255, 255, 255))
+
+		#inisialisasi objek
+		self.initGraph()
+
+		#membuat tampilan permainan
+		self.drawBoard()
+
+		self.drawBoard2()
+
+		#running program
+		flag1 = 0
+		flag2 = 0
+		x=0
+		while self.running:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					exit()
+				elif pygame.key.get_pressed()[pygame.K_SPACE]:
+						print "space"
+				elif pygame.key.get_pressed()[pygame.K_LEFT]:
+						print "left"
+				elif pygame.key.get_pressed()[pygame.K_RIGHT]:
+						print "right"	
+				#elif event.type == pygame.MOUSEBUTTONDOWN:
+				#	print "coord = (%d, %d)"%event.pos
+				elif event.type == pygame.MOUSEBUTTONDOWN and event.pos[0] < 228 and event.pos[1] < 275 and event.pos[1] > 50:
+					print "daun"
+					if x == 0:
+						flag1 = 1
+					elif x == 1:
+						flag2 = 1
+						self.screen.fill((255, 255, 255))
+						self.drawBoard3(flag1, flag2)
+					x+=1
+				elif event.type == pygame.MOUSEBUTTONDOWN and event.pos[0] > 227 and event.pos[0] < 463 and event.pos[1] < 275 and event.pos[1] > 50:
+					print "api"
+					if x == 0:
+						flag1 = 2
+					elif x == 1:
+						flag2 = 2
+						self.screen.fill((255, 255, 255))
+						self.drawBoard3(flag1, flag2)
+					x+=1
+				elif event.type == pygame.MOUSEBUTTONDOWN and event.pos[0] > 462 and event.pos[1] < 275 and event.pos[1] > 50:
+					print "air"
+					if x == 0:
+						flag1 = 3
+					elif x == 1:
+						flag2 = 3
+						self.screen.fill((255, 255, 255))
+						self.drawBoard3(flag1, flag2)
+					x+=1
+
+	def initGraph(self):
+		self.daun = pygame.image.load("daun.png").convert_alpha()
+		self.air = pygame.image.load("air.jpg").convert_alpha()
+		self.api = pygame.image.load("api.jpg").convert_alpha()
+		self.panel = pygame.image.load("score_panel.png").convert_alpha()
+		self.red = pygame.image.load("redindicator.png").convert_alpha()		
+		self.green = pygame.image.load("greenindicator.png").convert_alpha()
+
+	def drawBoard(self):
+		myfont = pygame.font.SysFont("monospace", 40)
+		label = myfont.render("Menunggu musuh . . .", 1, (0,0,0))
+		self.screen.blit(label, (0, 0))
+
+		self.screen.blit(self.daun, (0,50))
+		self.screen.blit(self.api, (230,50))
+		self.screen.blit(self.air, (465,50))
+		#self.screen.blit(self.panel, (140, 285))
+		self.screen.blit(self.red, (305, 300))
+		pygame.display.flip()
+		sleep(3)
+		self.screen.fill((255,255,255))
+
+	def drawBoard2(self):
+		myfont = pygame.font.SysFont("monospace", 40)
+		label = myfont.render("Pilih elemen anda . . .", 1, (0,0,0))
+		self.screen.blit(label, (0, 0))
+
+		self.screen.blit(self.daun, (0,50))
+		self.screen.blit(self.api, (230,50))
+		self.screen.blit(self.air, (465,50))
+		#self.screen.blit(self.panel, (140, 285))
+		self.screen.blit(self.green, (305, 300))
+		pygame.display.flip()
+
+element = Elemental()
