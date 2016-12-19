544a0fd38d9a334ab916a46424bd0337c024e876
dwikasm <dwikasm@gmail.com>
2016-12-20 03:17:25 +0700

    game udah jadi, masih bisa dikembangin lagi kalo mau

---
+++ fix/client.py
@@ -0,0 +1,219 @@
+from PodSixNet.Connection import ConnectionListener, connection
+from threading import Thread
+import pygame
+from time import sleep, time
+
+
+class Elemental(ConnectionListener):
+    def __init__(self, host, port):
+        self.gameid = None
+        self.running = False
+        self.Connect((host, port))
+        self.Load1()
+
+    def Loop(self):
+        connection.Pump()
+        self.Pump()
+
+    def Load1(self):
+        # membuat kotak
+        pygame.init()
+        self.screen = pygame.display.set_mode((690, 400))
+        pygame.display.set_caption("Elemental Fight")
+        self.clock = pygame.time.Clock()
+
+        # inisialisasi
+        # self.running = True
+
+        # inisialisasi posisi client ketika klik element
+        self.xpos = -1
+        self.ypos = -1
+
+        # membuat 60 fps
+        self.clock.tick(60)
+
+        # self.tunggu()
+
+        # warna bg
+        self.screen.fill((255, 255, 255))
+
+        # inisialisasi objek
+        self.initGraph()
+
+        # membuat tampilan permainan
+        if not self.running:
+            thread = Thread(target=self.drawBoard())
+            thread.start()
+
+        # untuk mengecek sudahkah ada musuh yang masuk
+        while not self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+            connection.Pump()
+            self.Pump()
+            # connection.Send({'action': 'pesan', 'pesan': 'pump it'})
+            sleep(0.01)
+        if self.running:
+            connection.Pump()
+            self.Pump()
+        # print 'mau load 2 neh'
+        self.Load2()
+
+    def Network_startgame(self, data):
+        self.playerid = data["player"]
+        self.running = True
+        self.gameid = data["gameid"]
+        connection.Send({'action': 'pesan', 'pesan': 'startgame'})
+
+    def Load2(self):
+        self.drawBoard2()
+        # running program
+        self.flag1 = None
+        self.flag2 = None
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                # elif event.type == pygame.MOUSEBUTTONDOWN:
+                # 	print "koordinat = (%d, %d)"%event.pos
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.pos[0] < 228 and event.pos[1] < 275 and \
+                                event.pos[
+                                    1] > 50:
+                    print "daun"
+                    self.flag1 = 1
+                    connection.Send(
+                        {"action": "pilih", "gameid": self.gameid, "flag": self.flag1, "player": self.playerid}
+                    )
+                    self.Loop()
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.pos[0] > 227 and event.pos[0] < 463 and \
+                                event.pos[
+                                    1] < 275 and event.pos[1] > 50:
+                    print "api"
+                    self.flag1 = 2
+                    connection.Send(
+                        {"action": "pilih", "gameid": self.gameid, "flag": self.flag1, "player": self.playerid}
+                    )
+                    self.Loop()
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.pos[0] > 462 and event.pos[1] < 275 and \
+                                event.pos[
+                                    1] > 50:
+                    print "air"
+                    self.flag1 = 3
+                    connection.Send(
+                        {"action": "pilih", "gameid": self.gameid, "flag": self.flag1, "player": self.playerid}
+                    )
+                    self.Loop()
+            # print 'flag 1 = ', self.flag1
+            # print 'flag 2 = ', self.flag2
+            self.Loop()
+            if (self.flag1 != None and self.flag2 != None):
+                self.Load3()
+
+    def Network_pilih(self, data):
+        if self.gameid == data['gameid']:
+            self.flag2 = data['flag']
+
+    def Load3(self):
+        # print 'flag 2 adalah ', self.flag2
+        self.screen.fill((255, 255, 255))
+        self.drawBoard3(self.flag1, self.flag2)
+
+    def Network_connected(self, data):
+        print "Berhasil terhubung ke server"
+
+    def Network_error(self, data):
+        print 'error:', data['error'][1]
+        connection.Close()
+
+    def Network_close(self, data):
+        exit()
+
+    def Network_disconnected(self, data):
+        print 'Server disconnected'
+        exit()
+
+    def initGraph(self):
+        self.daun = pygame.image.load("daun.png").convert_alpha()
+        self.air = pygame.image.load("air.jpg").convert_alpha()
+        self.api = pygame.image.load("api.jpg").convert_alpha()
+        self.panel = pygame.image.load("score_panel.png").convert_alpha()
+        self.red = pygame.image.load("redindicator.png").convert_alpha()
+        self.green = pygame.image.load("greenindicator.png").convert_alpha()
+
+    def drawBoard(self):
+        myfont = pygame.font.SysFont("monospace", 40)
+        label = myfont.render("Menunggu musuh . . .", 1, (0, 0, 0))
+        self.screen.blit(label, (0, 0))
+
+        self.screen.blit(self.daun, (0, 50))
+        self.screen.blit(self.api, (230, 50))
+        self.screen.blit(self.air, (465, 50))
+        # self.screen.blit(self.panel, (140, 285))
+        self.screen.blit(self.red, (305, 300))
+        pygame.display.flip()
+        # sleep(3)
+        self.screen.fill((255, 255, 255))
+
+    def drawBoard2(self):
+        myfont = pygame.font.SysFont("monospace", 40)
+        label = myfont.render("Pilih elemen anda . . .", 1, (0, 0, 0))
+        self.screen.blit(label, (0, 0))
+
+        self.screen.blit(self.daun, (0, 50))
+        self.screen.blit(self.api, (230, 50))
+        self.screen.blit(self.air, (465, 50))
+        # self.screen.blit(self.panel, (140, 285))
+        self.screen.blit(self.green, (305, 300))
+        pygame.display.flip()
+
+    def drawBoard3(self, flag1, flag2):
+        myfont = pygame.font.SysFont("monospace", 40)
+        label = myfont.render("Elemen anda: ", 1, (0, 0, 0))
+        label2 = myfont.render("Elemen musuh: ", 1, (0, 0, 0))
+        label3 = pygame.font.SysFont("monospace", 50).render("VS", 1, (0, 0, 0))
+        self.screen.blit(label, (0, 0))
+        self.screen.blit(label2, (345, 0))
+        self.screen.blit(label3, (300, 150))
+
+        if flag1 == 1:
+            self.screen.blit(self.daun, (40, 50))
+        elif flag1 == 2:
+            self.screen.blit(self.api, (40, 50))
+        elif flag1 == 3:
+            self.screen.blit(self.air, (40, 50))
+
+        if flag2 == 1:
+            self.screen.blit(self.daun, (400, 50))
+        elif flag2 == 2:
+            self.screen.blit(self.api, (400, 50))
+        elif flag2 == 3:
+            self.screen.blit(self.air, (400, 50))
+        self.screen.blit(self.panel, (140, 285))
+
+        if flag1 == 1 and flag2 == 1:
+            label5 = pygame.font.SysFont("monospace", 40).render("KALIAN SERI", 1, (255, 255, 255))
+        elif flag1 == 1 and flag2 == 2:
+            label5 = pygame.font.SysFont("monospace", 40).render("KAMU KALAH", 1, (255, 255, 255))
+        elif flag1 == 1 and flag2 == 3:
+            label5 = pygame.font.SysFont("monospace", 40).render("KAMU MENANG", 1, (255, 255, 255))
+        elif flag1 == 2 and flag2 == 1:
+            label5 = pygame.font.SysFont("monospace", 40).render("KAMU MENANG", 1, (255, 255, 255))
+        elif flag1 == 2 and flag2 == 2:
+            label5 = pygame.font.SysFont("monospace", 40).render("KALIAN SERI", 1, (255, 255, 255))
+        elif flag1 == 2 and flag2 == 3:
+            label5 = pygame.font.SysFont("monospace", 40).render("KAMU KALAH", 1, (255, 255, 255))
+        elif flag1 == 3 and flag2 == 1:
+            label5 = pygame.font.SysFont("monospace", 40).render("KAMU KALAH", 1, (255, 255, 255))
+        elif flag1 == 3 and flag2 == 2:
+            label5 = pygame.font.SysFont("monospace", 40).render("KAMU MENANG", 1, (255, 255, 255))
+        elif flag1 == 3 and flag2 == 3:
+            label5 = pygame.font.SysFont("monospace", 40).render("KALIAN SERI", 1, (255, 255, 255))
+        self.screen.blit(label5, (150, 290))
+        pygame.display.flip()
+
+host, port = "localhost", 8000
+element = Elemental(host, int(port))
+while 1:
+    element.Loop()
+    sleep(0.001)
---
+++ fix/server.py
@@ -0,0 +1,87 @@
+import PodSixNet.Channel
+import PodSixNet.Server
+from time import sleep
+
+
+class ClientChannel(PodSixNet.Channel.Channel):
+    def Network(self, data):
+        print data
+
+    def Network_pesan(self, data):
+        print "pesan: ", data['pesan']
+
+    def Network_pilih(self, data):
+        self.gameid = data["gameid"]
+        flag = data['flag']
+        print "pilihan dari ", data['player'], "adalah ", data['flag']
+        self._server.pilih(self.gameid, flag, data['player'])
+
+    def Close(self):
+        self._server.close(self.gameid)
+
+
+class MyServer(PodSixNet.Server.Server):
+    def __init__(self, *args, **kwargs):
+        PodSixNet.Server.Server.__init__(self, *args, **kwargs)
+        self.currentIndex = 0
+        self.games = []
+        self.queue = None
+
+    channelClass = ClientChannel
+
+    def Connected(self, channel, addr):
+        print 'koneksi baru: ', channel
+        # channel.Send({"action": "hello", "message": "hello client!"})
+        if self.queue == None:
+            self.currentIndex += 1
+            channel.gameid = self.currentIndex
+            self.queue = Game(channel, self.currentIndex)
+        else:
+            channel.gameid = self.currentIndex
+            self.queue.player1 = channel
+
+            self.queue.player0.Send(
+                {"action": "startgame", "player": 0, "gameid": self.queue.gameid}
+            )
+            self.queue.player1.Send(
+                {"action": "startgame", "player": 1, "gameid": self.queue.gameid}
+            )
+            self.games.append(self.queue)
+            self.queue = None
+
+    def pilih(self, gameid, flag, player):
+        game = [a for a in self.games if a.gameid == gameid]
+        if len(game) == 1:
+            game[0].pilih(gameid, flag, player)
+
+    def close(self, gameid):
+        try:
+            game = [a for a in self.games if a.gameid == gameid][0]
+            game.player0.Send({"action": "close"})
+            game.player1.Send({"action": "close"})
+        except:
+            pass
+
+
+class Game:
+    def __init__(self, player0, currentIndex):
+        self.player0 = player0
+        self.player1 = None
+        self.gameid = currentIndex
+
+    def pilih(self, gameid, flag, player):
+        if player == 1:
+            print 'flag(',flag,') dikirim ke player 0'
+            self.player0.Send({"action": "pilih", "flag": flag, "gameid": gameid})
+        else:
+            print 'flag(', flag, ') dikirim ke player 1'
+            self.player1.Send({"action": "pilih", "flag": flag, "gameid": gameid})
+
+
+host, port = "localhost", 8000
+myserver = MyServer(localaddr=(host, int(port)))
+if myserver:
+    print "Server nyala: ", host, ":", port
+while True:
+    myserver.Pump()
+    sleep(0.001)
